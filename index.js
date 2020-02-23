69031ac5906b95818b00cdeaa2c8b3cc2fa0a687
devantryx <45339768+devantryx@users.noreply.github.com>
2020-02-23 13:21:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ restService.post("/echo", function(req, res) {
     req.body.queryResult.parameters &&
     req.body.queryResult.parameters.echoText
       ? req.body.queryResult.parameters.echoText
-      : "Seems like some problem. Speak again.";
+      : "Parece un problema. Habla de nuevo.";
   
   var speechResponse = {
     google: {
